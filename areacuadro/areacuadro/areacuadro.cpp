200709e09e774875213bc75f159db2f15eac8f95
Alumno <Alumno@PC25>
2019-09-26 11:43:21 -0500

    chipresito

---
+++ areacuadro/areacuadro/areacuadro.cpp
@@ -0,0 +1,25 @@
+// areacuadro.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+#include <iostream>
+#include <string>	
+
+using namespace std;
+
+int main()
+{
+	cout << "==========================================================================\n";
+	cout << "||Bienvendio, este programa te permite calcular el area de un cuadrado.||\n";
+	cout << "==========================================================================\n";
+	float area;
+	float lado;
+
+	cout << "||Introduzca lado:                                                      ||\n";
+	cin >> lado;
+
+	area = lado * lado;
+
+	cout << "||El area del cuadrado es: " << area << "                                          ||\n";
+	cout << "==========================================================================\n";
+
+	return 0;
+}
